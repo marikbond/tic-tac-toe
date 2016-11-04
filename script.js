3270e16d8ca1373c517a69730eb4147b4617baff
Mark <markb.privat@gmail.com>
2016-11-04 12:08:44 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,72 @@
+
+var x = 'X';
+var o = 'O';
+var nextSymbol = x;
+
+var gameField = document.createElement('table');
+gameField.id = "game-field";
+gameField.onclick = fieldClickHandler;
+for (var i = 0; i < 3; i++) {
+    var tableRow = document.createElement('tr');
+    for (var j = 0; j < 3; j++) {
+        var td = document.createElement('td');
+        td.dataset.row = i;
+        td.dataset.col = j;
+        tableRow.appendChild(td);
+    }
+    gameField.appendChild(tableRow);
+}
+var gameFieldContainer = document.getElementById('game-field-container');
+gameFieldContainer.appendChild(gameField);
+
+function fieldClickHandler(event) {
+    var target = event.target;
+    while (target != gameField) {
+         if (target.tagName == 'TD') {
+             console.log('good job');
+             break;
+         }
+         target = target.parentNode;
+    }
+    console.log('row: ' + target.dataset.row + ' col: ' + target.dataset.col);
+    if (!target.innerHTML) {
+        target.innerHTML = nextSymbol;
+        nextSymbol = target.innerHTML == x ? o : x;
+        var victory = checkTheWin(target.innerHTML);
+        if (victory) {
+            handleWinner(target.innerHTML);
+        }
+    }
+    return false;
+}
+
+// TODO 2 на основании dataset написать алгоритм перебора ячеек для поиска победит
+function handleWinner(symbol) {
+    var spanId = symbol == x ? 'x-score' : 'o-score';
+    var span = document.getElementById(spanId);
+    span.innerHTML = +span.innerHTML + 1;
+    gameField.onclick = function () {
+        return false;
+    };
+    gameField.className = 'disabled-field';
+    alert('Win: ' + symbol);
+    nextSymbol = symbol;
+}
+
+var resetButton = document.getElementById('reset-button');
+resetButton.onclick = function () {
+    gameField.onclick = fieldClickHandler;
+    var tdList = gameField.getElementsByTagName('td');
+    for (var i = 0; i < tdList.length; i++) {
+        tdList[i].innerHTML = '';
+    }
+    gameField.className = '';
+};
+
+function checkTheWin(target) {
+    var rowNum = target.dataset.row;
+}
+
+
+
+
