6ed585648d9d92ef76a13dd48cb8c08cbd269ffd
Mark <markb.privat@gmail.com>
2016-12-20 18:23:31 -0500

    switch in the new function

--- views/gameFieldView.js
+++ views/gameFieldView.js
@@ -1,34 +1,35 @@
 
+function getCellSize(size) {
+    switch (size) {
+        case 3:
+            return 107;
+        case 9:
+            return 80;
+        case 15:
+            return 50;
+        case 30:
+            return 25;
+    }
+}
+
 function createGameField(size, onClickHandler) {
     var gameField = document.createElement('table');
     gameField.id = "game-field";
+    var cellSize = getCellSize(size);
     for (var i = 0; i < size; i++) {
         var tableRow = document.createElement('tr');
         for (var j = 0; j < size; j++) {
             var td = document.createElement('td');
             td.dataset.row = i;
             td.dataset.col = j;
+            td.style.width = cellSize;
+            td.style.height = cellSize;
             tableRow.appendChild(td);
         }
         gameField.appendChild(tableRow);
     }
-    switch (size) {
-        case 3:
-            td.style.width = '107px';
-            td.style.height = '107px';
-            break;
-        case 9:
-            td.style.width = '80px';
-            td.style.height = '80px';
-            break;
-        case 15:
-            td.style.width = '50px';
-            td.style.height = '50px';
-            break;
-        case 30:
-            td.style.width = '25px';
-            td.style.height = '25px';
-    }
+
+
     _('game-field-container').innerHTML = '';
     _('game-field-container').appendChild(gameField);
     gameField.onclick = onClickHandler;
